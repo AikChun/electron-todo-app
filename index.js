8841489c2435722a13b023e79275941bfdec44d6
Aik Chun <aikchun616@gmail.com>
2017-09-18 15:50:18 +0800

    Implemented menu option to create a new, smaller window.

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
 
     mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
+
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    });
+    
+}
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Tasks',
+                click() {
+                    createAddWindow();
+                }
+            },
+            { 
+                label: 'Quit',
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+W',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({});
+}
